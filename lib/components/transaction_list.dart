1b7a8654abe3e48c67a96b71ba68499a3b29f44a
Danilo Portela <daniloportela97@gmail.com>
2022-12-07 14:30:35 -0300

    Refatorar transection list.

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import './transaction_item.dart';
 import '../models/transaction.dart';
-import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final void Function(String) onRemove;
 
-  TransactionList(this.transactions, this.onRemove);
+  const TransactionList(this.transactions, this.onRemove, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,21 +15,17 @@ class TransactionList extends StatelessWidget {
         ? LayoutBuilder(
             builder: (ctx, constraints) {
               return Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: 20,
-                  ),
+                children: [
+                  const SizedBox(height: 20),
                   Text(
-                    'Nenhuma Transação Cadastrada.',
+                    'Nenhuma Transação Cadastrada!',
                     style: Theme.of(context).textTheme.headline6,
                   ),
+                  const SizedBox(height: 20),
                   SizedBox(
-                    height: 20,
-                  ),
-                  Container(
                     height: constraints.maxHeight * 0.6,
-                    child: Image(
-                      image: AssetImage('assets/images/waiting.png'),
+                    child: Image.asset(
+                      'assets/images/waiting.png',
                       fit: BoxFit.cover,
                     ),
                   ),
@@ -41,46 +37,9 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              return Card(
-                elevation: 5,
-                margin: EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5,
-                ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.purple,
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6),
-                      child: FittedBox(
-                        child: Text(
-                          'R\$${tr.value}',
-                        ),
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    tr.title,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  subtitle: Text(
-                    DateFormat('d MMM y').format(tr.date),
-                  ),
-                  trailing: MediaQuery.of(context).size.width > 480
-                      ? TextButton.icon(
-                          onPressed: () => onRemove(tr.id),
-                          icon: Icon(Icons.delete),
-                          label: Text('Excluir',
-                              style: TextStyle(
-                                color: Theme.of(context).errorColor,
-                              )))
-                      : IconButton(
-                          icon: Icon(Icons.delete),
-                          color: Theme.of(context).errorColor,
-                          onPressed: () => onRemove(tr.id),
-                        ),
-                ),
+              return TransactionItem(
+                tr: tr,
+                onRemove: onRemove,
               );
             },
           );
